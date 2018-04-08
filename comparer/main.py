c51e08256c297418c14a0978bace523d8351febe
DANIEL-PC\daniel <swaab.daniel@gmail.com>
2018-04-08 20:45:58 +0200

    code working, starting refactor/deleting

--- comparer/main.py
+++ comparer/main.py
@@ -1,5 +1,6 @@
 import data_generation
 import runTest
+import datetime
 
 MIRET = 'Miret'
 LAKH = 'Lakh'
@@ -9,16 +10,18 @@ TEST_LAKH_QUERY_TARGET_INNER_PATH= '??'
 TEST_MIRET_TARGET_INNER_PATH = 'QBT/REFMIDI/'
 TEST_MIRET_QUERY_INNER_PATH = 'QBT/onset/'
 TEST_MIRET_QUERY_TARGET_INNER_PATH = 'QBT/query.list.txt'
+TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH = 'QBt/test_query_list.txt'
 
 def genDataFiles():
-    data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_MIRET_QUERY_TARGET_INNER_PATH)
+    # data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_MIRET_QUERY_TARGET_INNER_PATH)
+    data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH)
     # data_generation.createIndexFiles(LAKH)
 
 def testToRun():
-    # runTest.execute(MIRET)
-    a = runTest.testCombination(MIRET)
-    print a
+    topTen, reciprocal = runTest.execute(MIRET)
+    print topTen, reciprocal
 
 genDataFiles()
+print datetime.datetime.now()
 res = testToRun()
-print res
+print datetime.datetime.now()
--- comparer/runTest.py
+++ comparer/runTest.py
@@ -24,19 +24,4 @@ def execute(test_set_name):
             resultMap[target] = saa.compareTracks(query_ioi, target_ioi)
 
         finalRes[query] = resultMap
-        break
     return calculateMetrics.calcMetrics(finalRes, queryTargetMap)
-
-def testCombination(test_set_name):
-    fileBase = data_generation.ROOT_DIR + data_generation.DATA_PATH
-    query_path = data_generation.QUERY_VECTOR_MAP + test_set_name + data_generation.JSON
-    target_path = data_generation.TARGET_VECTOR_MAP + test_set_name + data_generation.JSON
-    query_target_map_path = data_generation.QUERY_TARGET_MAP + test_set_name + data_generation.JSON
-    queryVectorMap = json.load(open(fileBase + query_path))
-    targetVectorMap = json.load(open(fileBase + target_path))
-    query = data_generation.ROOT_DIR + 'QBT/onset/0287.onset'
-    target = data_generation.ROOT_DIR +  'QBT/REFMIDI/0033.mid'
-    query_ioi = np.array(queryVectorMap[query]).astype(np.float)
-    target_ioi = np.array(targetVectorMap[target])
-    result = saa.compareTracks(query_ioi, target_ioi)
-    return result
