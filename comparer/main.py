a897f8552e8c343f275954be5dbbd9de97fcd0e1
DANIEL-PC\daniel <swaab.daniel@gmail.com>
2018-04-09 16:22:52 +0200

    Small test set + Smal speed up inside M matrix

--- comparer/main.py
+++ comparer/main.py
@@ -10,18 +10,18 @@ TEST_LAKH_QUERY_TARGET_INNER_PATH= 'Lakh/queryTargetMapLakh.csv'
 TEST_MIRET_TARGET_INNER_PATH = 'QBT/REFMIDI/'
 TEST_MIRET_QUERY_INNER_PATH = 'QBT/onset/'
 TEST_MIRET_QUERY_TARGET_INNER_PATH = 'QBT/query.list.txt'
-TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH = 'QBt/test_query_list.txt'
+TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH = 'QBt/test_query_list_short.txt'
 
 def genDataFiles():
     # data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_MIRET_QUERY_TARGET_INNER_PATH)
-    # data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH)
-    data_generation.createLakhFiles(LAKH, TEST_LAKH_QUERY_INNER_PATH, TEST_LAKH_TARGET_FULL_PATH, TEST_LAKH_QUERY_TARGET_INNER_PATH)
+    data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH)
+    # data_generation.createLakhFiles(LAKH, TEST_LAKH_QUERY_INNER_PATH, TEST_LAKH_TARGET_FULL_PATH, TEST_LAKH_QUERY_TARGET_INNER_PATH)
 
 def testToRun():
-    topTen, reciprocal = runTest.execute(LAKH)
+    topTen, reciprocal = runTest.execute(MIRET)
     print topTen, reciprocal
 
-# genDataFiles()
+genDataFiles()
 print datetime.datetime.now()
 res = testToRun()
 print datetime.datetime.now()
