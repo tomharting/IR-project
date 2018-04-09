8f20ab30e9ba84ddb5d9b6913857e48d4ea5ee35
DANIEL-PC\daniel <swaab.daniel@gmail.com>
2018-04-09 23:09:07 +0200

    Run QBT_symbolic

--- comparer/main.py
+++ comparer/main.py
@@ -4,21 +4,30 @@ import datetime
 
 MIRET = 'Miret'
 LAKH = 'Lakh'
+QBTS = 'QBTS'
+
 TEST_LAKH_QUERY_INNER_PATH= 'Lakh/all_user_query/'
 TEST_LAKH_TARGET_FULL_PATH= 'C:/Users/daniel.DANIEL-PC/Documents/uni/Master/Information retrieval/group project/clean_midi/'
 TEST_LAKH_QUERY_TARGET_INNER_PATH= 'Lakh/queryTargetMapLakh.csv'
+
 TEST_MIRET_TARGET_INNER_PATH = 'QBT/REFMIDI/'
 TEST_MIRET_QUERY_INNER_PATH = 'QBT/onset/'
 TEST_MIRET_QUERY_TARGET_INNER_PATH = 'QBT/query.list.txt'
+
+TEST_QBTS_TARGET_INNER_PATH = 'QBT_symbolic/Midi/'
+TEST_QBTS_QUERY_INNER_PATH = 'QBT_symbolic/Onset query/'
+TEST_QBTS_QUERY_TARGET_INNER_PATH = 'QBT_symbolic/answer.txt'
+
 TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH = 'QBt/test_query_list.txt'
 
 def genDataFiles():
-    data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_MIRET_QUERY_TARGET_INNER_PATH)
+    # data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_MIRET_QUERY_TARGET_INNER_PATH)
+    data_generation.createIndexFilesQBTS(QBTS, TEST_QBTS_QUERY_INNER_PATH, TEST_QBTS_TARGET_INNER_PATH, TEST_QBTS_QUERY_TARGET_INNER_PATH)
     # data_generation.createIndexFiles(MIRET, TEST_MIRET_QUERY_INNER_PATH, TEST_MIRET_TARGET_INNER_PATH, TEST_TEST_MIRET_QUERY_TARGET_INNER_PATH)
     # data_generation.createLakhFiles(LAKH, TEST_LAKH_QUERY_INNER_PATH, TEST_LAKH_TARGET_FULL_PATH, TEST_LAKH_QUERY_TARGET_INNER_PATH)
 
 def testToRun():
-    topTen, reciprocal = runTest.execute(MIRET)
+    topTen, reciprocal = runTest.execute(QBTS)
     print topTen, reciprocal
 
 genDataFiles()
